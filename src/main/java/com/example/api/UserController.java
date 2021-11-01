0019029a3bf55dbe4263c2302323cc8cee81c9d0
nadia-baa <nadiabarakat678@gmail.com>
2021-11-01 09:23:44 +0100

    add thymleaf

--- src/main/java/com/example/api/UserController.java
+++ src/main/java/com/example/api/UserController.java
@@ -7,25 +7,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+
 //@RestController
+@Controller
 public class UserController {
 	
 	@Autowired
 	private UserService US;
 	
 	
-	/* @GetMapping("/user")
-	    public Iterable<User> getUsers() {
-	        return US.getUsers();
-	    }*/
+	 @GetMapping("/inscrire")
+	    public String getUsers() {
+	        return "inscrire";
+	 }
 	 
-	 @PostMapping("")
-	 public User addUser(User user) {
-			return US.addUser(user);		
+	 @PostMapping("/adduser")
+	 public ModelAndView creatUser(@ModelAttribute User user) {
+		    US.addUser(user);
+		    return new ModelAndView("redirect:/");	
 		}
 
 	 @GetMapping("/sup")
